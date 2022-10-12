4e8aa54e86fcf2bb0d97cd9ecf6f7f514766d5b0
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2022-10-12 11:10:10 -0300

    busca cep cli

---
+++ 13-busca-cep/go.mod
@@ -0,0 +1,3 @@
+module 13-busca-cep
+
+go 1.19
---
+++ 13-busca-cep/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type BuscaCepResultado struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	for _, cep := range os.Args[1:] {
+		fmt.Println(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+
+		req, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao fazer requisição: %v\n", err)
+		}
+		defer req.Body.Close()
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao ler resposta: %v\n", err)
+		}
+
+		var data BuscaCepResultado
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao fazer parse da resposta: %v\n", err)
+		}
+
+		// If the file doesn't exist, create it, or append to the file
+		file, err := os.OpenFile("cidade.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "erro ao criar arquivo: %v\n", err)
+		}
+		defer file.Close()
+
+		if _, err := file.Write([]byte(fmt.Sprintf("-----------------\nCEP: %s,\nLocalidade: %s,\nUF: %s\n", data.Cep, data.Localidade, data.Uf))); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(data)
+	}
+}
