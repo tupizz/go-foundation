faf549a30c7d6620976e38bc829a6292802c9dfb
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2022-10-17 21:41:11 -0300

    go database crud

--- 31-database/main.go
+++ 31-database/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
+	"time"
 )
 
 type Product struct {
@@ -50,6 +53,54 @@ func insertProduct(db *sql.DB, product *Product) error {
 	return nil
 }
 
+func selectOneProduct(ctx context.Context, db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+
+	//err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price) // outra forma de fazer
+	// no scan devemos passar na mesma ordem a qual fizemos o select
+	err = stmt.QueryRowContext(ctx, id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
+}
+
+func deleteProduct(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("delete from products where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
 	if err != nil {
@@ -58,7 +109,7 @@ func main() {
 	defer db.Close()
 
 	// criar produto
-	product := NewProduct("xbox", 1850.99)
+	product := NewProduct("playstation 5", 3850.99)
 	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
@@ -66,10 +117,31 @@ func main() {
 
 	// update produto
 	product.Price = 2099
-	product.Name = "Xbox novo updated"
+	product.Name = "Play 5 novo updated"
 	err = updateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	productSelected, err := selectOneProduct(ctx, db, "2d3608d0-7d82-4bee-ac67-08ac2519b142")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(productSelected)
+
+	fmt.Println("------------------------------------------")
+
+	products, err := selectAllProducts(db)
+	for _, p := range products {
+		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
+
+	fmt.Println("------------------------------------------")
+
+	err = deleteProduct(db, products[0].ID)
+	if err != nil {
+		panic(err)
+	}
 }
